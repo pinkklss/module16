1c9e8de556e407922ae1a16de0e3cce3756dd1db
pinkklss <shaker_04@mail.ru>
2024-11-17 21:32:21 +0700

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, status, Body, HTTPException, Path
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
+from typing import Annotated
 
 app = FastAPI()
 
@@ -8,37 +9,42 @@ users = []
 
 
 class User(BaseModel):
-    id: int
+    id: int = None
     username: str
     age: int
 
 
-@app.get('/users', response_model=List[User])
-def get_users():
+@app.get("/users")
+def get_all_users() -> List[User]:
     return users
 
 
-@app.post('/user/{username}/{age}', response_model=User)
-def create_user(username: str, age: int):
-    user_id = 1 if not users else users[-1].id + 1  # Генерация нового id
-    new_user = User(id=user_id, username=username, age=age)
-    users.append(new_user)
-    return new_user
+@app.post("/user/{username}/{age}")
+def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+    user_id = (users[-1].id + 1) if users else 1
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
 
 
-@app.put('/user/{user_id}/{username}/{age}', response_model=User)
-def update_user(user_id: int, username: str, age: int):
+@app.put("/user/{user_id}/{username}/{age}")
+def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],
+                username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
     for user in users:
         if user.id == user_id:
             user.username = username
             user.age = age
             return user
-    raise HTTPException(status_code=404, detail="User was not found")
+    else:
+        raise HTTPException(status_code=404, detail='User was not found')
 
 
-@app.delete('/user/{user_id}', response_model=User)
-def delete_user(user_id: int):
-    for index, user in enumerate(users):
-        if user.id == user_id:
-            return users.pop(index)
-    raise HTTPException(status_code=404, detail="User was not found")
+@app.delete("/user/{user_id}")
+def delete_user(user_id: int) -> str:
+    try:
+        users.pop(user_id)
+        return f"User id = {user_id} deleted"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User not found")
